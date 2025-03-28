44f3a686577a2f30eab09166513e969bc76d0950
AresChris <148880863+AresChris@users.noreply.github.com>
2025-03-28 11:46:10 +0100

    MAJ 28/03/25
    MAJ 28/03/25

--- DWWM/C#/Exercices C#/exercice_2.1.2/exercice_2.1.2/obj/Debug/net8.0/exercice_2.1.2.AssemblyInfo.cs
+++ DWWM/C#/Exercices C#/exercice_2.1.2/exercice_2.1.2/obj/Debug/net8.0/exercice_2.1.2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exercice_2.1.2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69edba9a8d14e907d1c58a27a101097bf1a12a83")]
 [assembly: System.Reflection.AssemblyProductAttribute("exercice_2.1.2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exercice_2.1.2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
