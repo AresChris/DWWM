55a0a40a3ab22d0d3588fcbcabda4bfbae21b9b5
AresChris <148880863+AresChris@users.noreply.github.com>
2025-05-14 16:55:02 +0200

    MAJ 14/05/25

---
+++ C#/Exercices C#/bouteille/Bouteille.cs
@@ -0,0 +1,112 @@
+﻿namespace ClassLibraryBouteille
+{
+    public class Bouteille
+    {
+        private bool ouverte;
+        private string nom;
+        private decimal contenance;
+        private decimal contenu;
+
+        //constructeur par default
+        public Bouteille()
+        {
+            this.ouverte = false;
+            this.nom = "bout";
+            this.contenance = 1;
+            this.contenu = 1;
+        }
+
+        //classique
+        public Bouteille(bool ouverte,string nom, int contenance, int contenu)
+        {
+            this.ouverte = ouverte; 
+            this.nom = nom;
+            this.contenance = contenance;
+            this.contenu = contenu;
+        }
+
+        public Bouteille(Bouteille bouteilleACopier)
+        {
+            this.ouverte = bouteilleACopier.ouverte;
+            this.nom = bouteilleACopier.nom;
+            this.contenance = bouteilleACopier.contenance;
+            this.contenu = bouteilleACopier.contenu;
+        }
+
+        //clonage
+        public override string ToString()
+        {
+            //    string description = $" est ouverte : {this.estOuvert} /Sa contenance en litre : {this.contenanceEnLitre} /Son contenu en litre : {this.contenuEnLitre}";
+
+            return base.ToString() + " : " + nameof(ouverte) + " = " + ouverte + " : " + nameof(contenance) + " = " + contenance + " : " + nameof(contenu) + " = " + contenu;
+        }
+        public bool Ouvrir()
+        {
+            if (ouverte)
+            {
+                return false;
+            }
+            else
+            {
+                ouverte = true;
+                return true;
+            }
+        }
+
+        public bool Fermer()
+        {
+            if (!ouverte)
+            {
+                return false;
+            }
+            else
+            {
+                ouverte = false;
+                return true;
+            }
+        }
+        public bool Remplir(decimal quantiteAremplir)
+        {
+            if (quantiteAremplir < 0)
+            {
+                throw new ArgumentOutOfRangeException("La quantité à remplir ne peut pas être négative.",nameof(quantiteAremplir));
+            }
+            if (!ouverte)
+            {
+                return false;
+            }
+            else
+            {
+                if ( quantiteAremplir <= (contenance - contenu))
+                {
+                    contenu = quantiteAremplir + contenu;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        public bool Vider(decimal quantiteAVider)
+        {
+            if (quantiteAVider < 0)
+            {
+                throw new ArgumentOutOfRangeException("La quantité à vider ne peut pas être négative.", nameof(quantiteAVider));
+            }
+            if (!ouverte)
+            {
+                return false;
+            }
+            else if (quantiteAVider >= 0 && quantiteAVider <= contenu)
+            {
+                contenu = contenu - quantiteAVider;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ C#/Exercices C#/bouteille/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using ClassLibraryBouteille;
+using System.Collections.Generic;
+using ClassLibraryBouteille;
+
+namespace la_bouteille
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bouteille cristalline = new Bouteille(false, "cristalline", 100, 50);
+            List<Bouteille> bouteilles = new List<Bouteille> { cristalline };
+
+            //--------------------------------------
+            cristalline.Ouvrir();
+            // Bouteille ouverte
+            cristalline.Ouvrir();
+            Console.WriteLine(cristalline.ToString());
+
+            cristalline.Vider(10);
+            Console.WriteLine(cristalline.ToString());
+
+            cristalline.Remplir(10);
+            Console.WriteLine(cristalline.ToString());
+
+            cristalline.Fermer();
+            Console.WriteLine(cristalline.ToString());
+
+            //--------------------------------------
+            // Bouteille fermée
+            cristalline.Vider(10);
+            Console.WriteLine(cristalline.ToString());
+
+            cristalline.Remplir(10);
+            Console.WriteLine(cristalline.ToString());
+
+            cristalline.Fermer();
+            Console.WriteLine(cristalline.ToString());
+
+            cristalline.Ouvrir();
+            Console.WriteLine(cristalline.ToString());
+
+            //--------------------------------------
+            cristalline.Remplir(50);
+            // Bouteille pleine
+            cristalline.Ouvrir();
+            Console.WriteLine(cristalline.ToString());
+
+            cristalline.Remplir(10);
+            Console.WriteLine(cristalline.ToString());
+
+            cristalline.Fermer();
+            Console.WriteLine(cristalline.ToString());
+
+            cristalline.Vider(10);
+            Console.WriteLine(cristalline.ToString());
+
+            //--------------------------------------
+            cristalline.Vider(50);
+            //Bouteille vide
+            cristalline.Ouvrir();
+            Console.WriteLine(cristalline.ToString());
+
+            cristalline.Vider(10);
+            Console.WriteLine(cristalline.ToString());
+
+            cristalline.Fermer();
+            Console.WriteLine(cristalline.ToString());
+
+            cristalline.Remplir(10);
+            Console.WriteLine(cristalline.ToString());
+        }
+    }
+}
