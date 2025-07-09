8208041e3bb5d8933132af916aeaec4cf431c96f
AresChris <148880863+AresChris@users.noreply.github.com>
2025-07-09 10:35:23 +0200

    Maj Jalon

---
+++ HTML/Exercices HTML/Basic Template/style/main.js
@@ -0,0 +1,9 @@
+const liste = getElementById("liste");
+
+burger.addEventListener("click", function() {
+    if (menu.style.display === "none") {
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
+    }
+});
